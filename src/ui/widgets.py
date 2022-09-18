84e41fae6f5a304a335eacee2236bdba15818c3e
Zachary Cowan <44091329+zacowan@users.noreply.github.com>
2022-09-17 22:53:58 -0400

    feat(ui): add calibration window

--- src/ui/widgets.py
+++ src/ui/widgets.py
@@ -9,6 +9,12 @@ class MainWidget(QMainWindow):
     width = 360
     height = 240
 
+    @QtCore.Slot()
+    def open_calibration_window(self):
+        self.calibration_window = CalibrationWidget()
+        self.calibration_window.showFullScreen()
+        self.showMinimized()
+
     def __init__(self, primary_screen: QScreen):
         super().__init__()
 
@@ -23,17 +29,19 @@ class MainWidget(QMainWindow):
             "background-color: black; color: white")
 
         # Create placeholder text
-        text = QLabel("Main Window",
-                      alignment=QtCore.Qt.AlignCenter)
+        self.text = QLabel("Main Window",
+                           alignment=QtCore.Qt.AlignCenter)
         # Create calibrate button
-        calibrate_button = QPushButton("Calibrate")
+        self.calibrate_button = QPushButton("Calibrate")
+        # Connect onClick
+        self.calibrate_button.clicked.connect(self.open_calibration_window)
 
         # Create layout container
         layout = QVBoxLayout()
         # Add placeholder text to container
-        layout.addWidget(text)
+        layout.addWidget(self.text)
         # Add calibrate button to container
-        layout.addWidget(calibrate_button)
+        layout.addWidget(self.calibrate_button)
 
         # Create main window
         central_widget = QWidget()
@@ -47,3 +55,30 @@ class MainWidget(QMainWindow):
         ) - MainWidget.width, 0, MainWidget.width, MainWidget.height)
         # Lock the width and height of the window
         self.setFixedSize(MainWidget.width, MainWidget.height)
+
+
+class CalibrationWidget(QMainWindow):
+    """Full-screen window with calibration steps."""
+
+    def __init__(self):
+        super().__init__()
+
+        # Remove window title
+        self.setWindowTitle("Iris Software - Calibration")
+
+        # Create placeholder text
+        self.text = QLabel("Calibration Window",
+                           alignment=QtCore.Qt.AlignCenter)
+
+        # Create layout container
+        layout = QVBoxLayout()
+        # Add placeholder text to container
+        layout.addWidget(self.text)
+
+        # Create main window
+        central_widget = QWidget()
+        # Add container to main window
+        central_widget.setLayout(layout)
+
+        # Set the main window
+        self.setCentralWidget(central_widget)
