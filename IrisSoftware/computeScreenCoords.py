e82ae5d68ca22fbe244bac56076cf2e12e676525
Nalin Mehra <nalinm01@gmail.com>
2022-10-17 22:28:20 -0400

    refactor(computeCoords): refactor linear regression interpolator

--- IrisSoftware/computeScreenCoords.py
+++ IrisSoftware/computeScreenCoords.py
@@ -3,10 +3,14 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 import pandas as pd 
 from scipy.interpolate import LinearNDInterpolator
-
+import os
+import pickle
+from ui import CALIBRATION_FILE_NAME
 class InterpolationType(Enum):
     LINEAR = 1
     RBF_LINEAR = 2
+    LINEAR_REGRESSION = 3
+    
 
 def unpackEyeCoords(eyeCoords: list[list[tuple]]) -> list[tuple]:
     # unpacks [[(1,2),(3,4)], [(5,6), (7,8)]] to [(1,2,3,4), (5,6,7,8)]
@@ -16,7 +20,7 @@ def unpackScreenCoords(screenCoords):
     screenXCoords, screenYCoords = zip(*screenCoords)
     return (list(screenXCoords), list(screenYCoords))
 
-class LinearRegression():
+class LinearRegressionInterpolator():
     def __init__(self, eyeCoords: list[tuple], screenXCoords: list, screenYCoords: list):
         df_X = pd.DataFrame(eyeCoords)
         # TODO: restructure to perform unpacking in models instead of beforehand to account for differences in implementations
@@ -35,25 +39,31 @@ class LinearInterpolator():
     def computeScreenCoords(self, eyeCoords):
         return (self.xInterpolator(eyeCoords), self.yInterpolator(eyeCoords))
 
-class RBFInterpolator():
-    # def __init__(self, eyeCoords: List[Tuple], screenXCoords: List, screenYCoords: List, kernel=None, smoothing=None):
-    #     self.kernel = kernel
-    #     self.smoothing = smoothing
-    #     self.rbf = RBFInterpolator()
-    pass
-
 class Interpolator():
-    def __init(self):
+    def __init__(self):
         self.interpolator = None
 
-    def calibrateInterpolator(self, eyeCoords: list[list[tuple]], screenCoords: list[tuple], interpType = InterpolationType.LINEAR):
+    def calibrateInterpolatorManual(self, eyeCoords: list[list[tuple]], screenCoords: list[tuple], interpType = InterpolationType.LINEAR_REGRESSION):
         # eyeCoords of shape [[(x1.1,y1.1),(x1.2,y1.2)], [(x2.1,y2.1, x2.2, y2.2)], etc.]
         # screenCoords of shape [(x1,y1), (x2,y2), (x3,y3), etc]
         if interpType == InterpolationType.LINEAR:
             unpackedEyeCoords = unpackEyeCoords(eyeCoords)
             unpackedXScreenCoords, unpackedYScreenCoords = unpackScreenCoords(screenCoords)
             self.interpolator = LinearInterpolator(unpackedEyeCoords, unpackedXScreenCoords, unpackedYScreenCoords)
+        elif interpType == InterpolationType.LINEAR_REGRESSION:
+            self.interpolator = LinearRegressionInterpolator(eyeCoords, screenCoords)
+        elif interpType == InterpolationType.LOGISTIC_REGRESSION:
+            self.interpolator = LinearRegressionInterpolator(eyeCoords, screenCoords)
         return
+    def calibrateInterpolator(self, calibration_file = CALIBRATION_FILE_NAME, interpType = InterpolationType.LINEAR_REGRESSION):
+        if not os.path.exists(calibration_file):
+            raise ValueError('Error when calibrating interpolator')
+
+        with open(calibration_file, 'rb') as calibration_data:
+            calibrationData = pickle.load(calibration_data)
+        self.calibrateInterpolatorManual(calibrationData['eyeCoords'], calibrationData['calibrationCircleLocations'], interpType)
+
+
     
     def computeScreenCoords(self, eyeCoords) -> tuple:
         # eyeCoords of shape [x1,y1,x2,y2]
