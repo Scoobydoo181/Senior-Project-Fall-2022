4d2656c5f83d0f9592366eb8753ffe7311c101dd
Scoobydoo181 <44757478+Scoobydoo181@users.noreply.github.com>
2022-10-24 17:28:59 -0400

    Update main.py

--- IrisSoftware/main.py
+++ IrisSoftware/main.py
@@ -26,7 +26,7 @@ class IrisSoftware:
             self.faceBox = None
             self.lastCursorPos = pyautogui.position()
             self.skipMouseMovement = False
-            self.interpolatorType = InterpolationType.TANGENT_LINEAR_REGRESSION # InterpolationType.TANGENT_LINEAR_REGRESSION
+            self.interpolatorType = InterpolationType.TANGENT_LINEAR_REGRESSION
 
     def __init__(self) -> None:
         print("Initializing Iris Software...")
